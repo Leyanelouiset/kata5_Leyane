4ab567ab846f545b09b9b6411cadb48ff4d634b2
leyanelouiset <louisetleyane@gmail.com>
2025-03-24 14:33:26 +0100

    server+route

--- back_end/server.js
+++ back_end/server.js
@@ -1,10 +1,15 @@
-import express from 'express';
+import express from 'express'; //un serveur est un ordinateur qui ecoute
 //import { router } from './routes.js';
 
 const app = express();
+
 app.use(express.json());
 
 app.use(router);
+
+app.get("/", (req,res) =>{
+    res.send("bonjour")
+});
 
 app.listen(3000, async () => {
 console.log(`Server running on http://localhost:3000`);
